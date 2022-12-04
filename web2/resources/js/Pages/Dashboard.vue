87ac2fa3324d36c04abf6e12fbd7b4a6edfaf13a
Nasc1mento <reisadryan16@gmail.com>
2022-12-04 00:50:13 -0300

    add vue, create model, create migrations

---
+++ web2/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    products:[]
+});
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+                <div>
+                    <ul>
+                        <li v-for="product in products">{{ product.title }}</li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    </AuthenticatedLayout>
+</template>
